5b0af3e412d70d003230705438d695951e3665fb
Narasimha Reddy <narasimhareddy207y@gmail.com>
2025-06-02 16:10:44 +0530

    Add files via upload

---
+++ AnagramCheck.java
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String str1 = sc.nextLine();
+
+        System.out.print("Enter second string: ");
+        String str2 = sc.nextLine();
+
+        // Convert to char arrays
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        // Sort the arrays
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        // Compare sorted arrays
+        boolean isAnagram = Arrays.equals(arr1, arr2);
+
+        System.out.println("Are anagrams: " + isAnagram);
+
+        sc.close();
+    }
+}
